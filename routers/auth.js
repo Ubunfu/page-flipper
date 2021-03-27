898a9f8cf3466c22eead567f9540a2bfc83c38db
Ryan Allen <rallen3882@gmail.com>
2021-03-27 02:08:28 -0400

    [RAllen] Writing signup and login forms

--- routers/auth.js
+++ routers/auth.js
@@ -1,4 +1,13 @@
 const express = require('express')
+const { route } = require('./main')
 const router = express.Router()
+
+router.get('/signup', (req, res) => {
+    res.render('signup', {})
+})
+
+router.get('/login', (req, res) => {
+    res.render('login', {})
+})
 
 module.exports = router
