4758ecad66ac66d38d25f0f1b552ee44bdc4b2b6
Ryan Allen <rallen3882@gmail.com>
2021-03-27 00:57:55 -0400

    [RAllen] init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Page Flipper
+Page Flipper is a place dedicated to enjoying your favorite books together with friends. Plan club meetings, propose book selections, vote on candidates, and leave your thoughts with ratings and reviews.
---
+++ app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// eslint-disable-next-line import/no-unresolved
+require('dotenv').config()
+const express = require('express');
+const path = require('path')
+const hbs = require('hbs')
+const session = require('express-session')
+const routers = require('./routers')
+
+const staticsPath = path.join(__dirname, './public')
+const partialsPath = path.join(__dirname, './views/partials')
+
+const app = express();
+app.set('view engine', 'hbs')
+hbs.registerPartials(partialsPath)
+app.use(express.static(staticsPath))
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(routers.main)
+app.use(routers.auth)
+
+if (process.env.LOCAL == 'true') {
+  app.listen(process.env.LOCAL_PORT, () => {
+      console.log('Server is up!');
+  })
+}
+
+module.exports = app;
