c4ac1e434149ed259674842b4a530c60bb9042b5
Ryan Allen <rallen3882@gmail.com>
2021-06-06 22:05:43 -0400

    Building club page with mock data

--- site/routers/club.js
+++ site/routers/club.js
@@ -25,4 +25,41 @@ router.post('/club/create', async (req, res) => {
     return res.redirect('/dashboard')
 })
 
+router.get('/club/:clubId', async (req, res) => {
+    const clubId = req.params.clubId
+    const club = await dbService.getClubById(clubId)
+    console.log(`[RTR] [GET /club/:clubId] ${JSON.stringify(club)}`);
+    return res.render('club', {
+        club,
+        isRequesterAdmin: true,
+        clubAdmins: [
+            {
+                firstName: "Ryan",
+                lastName: "Allen Mcguidwin Miramonti Keel"
+            },
+            {
+                firstName: "Angela",
+                lastName: "Goodwin"
+            }
+        ],
+        memberCount: 177,
+        meetings: [
+            {
+                meetingId: "blablahblah",
+                bookIconUrl: "https://img1.looper.com/img/gallery/the-director-of-it-wants-to-remake-stephen-kings-pet-sematary/intro-1505827169.jpg",
+                bookTitle: "Pet Semetary",
+                bookAuthor: "Stephen King",
+                meetingDate: "June 1st, 2021"
+            },
+            {
+                meetingId: "blablahblah",
+                bookIconUrl: "https://img1.looper.com/img/gallery/the-director-of-it-wants-to-remake-stephen-kings-pet-sematary/intro-1505827169.jpg",
+                bookTitle: "Pet Semetary",
+                bookAuthor: "Stephen King",
+                meetingDate: "June 1st, 2021"
+            }
+        ]
+    })
+})
+
 module.exports = router
